4648ef19221f2a6b2f4cc9f1ae1b73b5379e8385
sangki930 <sangki931@gmail.com>
2024-08-21 23:46:49 +0900

    ComplexityTest

--- Practice/src/algorithm/ComplexityTest.java
+++ Practice/src/algorithm/ComplexityTest.java
@@ -1,9 +1,9 @@
 package algorithm;
 
+// 복잡도 예시
 public class ComplexityTest {
     public static void main(String[] args) {
         int[] arr = new int[10000000];  // 큰 배열 생성
         int target = 999999;            // 찾을 인덱스 생성
-
     }
 }
