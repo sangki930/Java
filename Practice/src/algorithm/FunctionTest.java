28723351f850fd26c81d1d7d181b3d52d0309666
sangki930 <sangki931@gmail.com>
2024-09-04 23:34:15 +0900

    자바 연습4

--- Practice/src/algorithm/FunctionTest.java
+++ Practice/src/algorithm/FunctionTest.java
@@ -1,8 +1,17 @@
 package algorithm;
 
+import java.util.List;
+
 public class FunctionTest {
     public void test(){
 
     }
 
+    public void test(int i){
+
+    }
+
+    public int test(List<String> list){
+        return -1;
+    }
 }
