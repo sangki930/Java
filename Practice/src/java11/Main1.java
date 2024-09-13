6bb8b87a9098dce17ab7e8e3ad788f2fb279da50
sangki930 <sangki931@gmail.com>
2024-09-13 23:59:02 +0900

    자바 연습

--- Practice/src/java11/Main1.java
+++ Practice/src/java11/Main1.java
@@ -6,7 +6,6 @@ public class Main1 {
     public static void main(String[] args) {
         System.out.println("메인1");
     }
-
     // 동작 안함
     public void main(){
         System.out.println("메인2");
